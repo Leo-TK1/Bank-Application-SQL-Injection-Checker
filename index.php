ac146135593dbf30025d138d301ad5575f01ce93
ricokoro <77748744+ricokoro@users.noreply.github.com>
2023-03-24 02:06:10 -0400

    newest

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Banking Home</title>
+    <link rel="icon" type="image/x-icon" href="favicon.png">
+    
+    <!-- CSS -->
+    <!link rel="stylesheet" href="style.css">
+
+    <!-- Montesarat Font -->
+    <!link rel="preconnect" href="https://fonts.googleapis.com">
+    <!link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<h1>Welcome to our Bank</h1>
+	<div>
+		<button onclick="location.href='create_account.php'">Create Account</button>
+		<button onclick="location.href='login.php'">Log In</button>
+		<button onclick="location.href='admin_login.php'">Admin Login</button>
+	</div>
+</body>
